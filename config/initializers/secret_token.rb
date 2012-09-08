e14630fb74234e8c30bf8f0dcf7af5d01afe6094
Cowmanifestation <cowmanifestation@gmail.com>
2012-09-08 16:58:22 -0700

    A new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chipmunk::Application.config.secret_token = 'efed5351d43be9419f27775d37005f50d714641529fecefff88c7bf8ff6cc24eba50502d9c5aeb483c11e98f1bfbb89a8e0c737c072e13cf79c10b4384a7315e'
