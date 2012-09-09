32ef23bd5fb550f9886f2122471406e7e1db5897
Cowmanifestation <cowmanifestation@gmail.com>
2012-09-09 15:19:01 -0700

    Added dummy content to all pages

--- spec/requests/statics_spec.rb
+++ spec/requests/statics_spec.rb
@@ -9,4 +9,28 @@ describe "Statics" do
       page.should have_content 'Rich'
     end
   end
+
+  describe "About page" do
+
+    it "should have the content 'About Rich'" do
+      visit '/static/about'
+      page.should have_content 'About Rich'
+    end
+  end
+  
+  describe "Contact page" do
+
+    it "should have the content 'Contact Rich'" do
+      visit '/static/contact'
+      page.should have_content 'Contact Rich'
+    end
+  end
+
+  describe "Arts page" do
+
+    it "should have the content 'About the Arts'" do
+      visit '/static/arts'
+      page.should have_content 'About the Arts'
+    end
+  end
 end
